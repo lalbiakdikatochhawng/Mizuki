f45e11dd4458f7779e1e8bd7a7c10e950adfb3e0
lalbiakdikatochhawng <79501915+lalbiakdikatochhawng@users.noreply.github.com>
2021-03-30 16:25:54 +0530

    Update __main__.py

--- Mizuki/__main__.py
+++ Mizuki/__main__.py
@@ -622,7 +622,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am now online!")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Ka online leh thei chiah e!")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
