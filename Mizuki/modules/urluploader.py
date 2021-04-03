d4e85dec4e2c8dadc373626a20b7129b66d51109
lalbiakdikatochhawng <79501915+lalbiakdikatochhawng@users.noreply.github.com>
2021-04-03 13:36:58 +0530

    Update urluploader.py

--- Mizuki/modules/urluploader.py
+++ Mizuki/modules/urluploader.py
@@ -32,7 +32,7 @@ async def download_coroutine(session, url, file_name, event, start, bot):
 
 **File Size:** {}
 
-**© @Infinity_BOTs**""".format(
+**© @puituflynn**""".format(
                 url,
                 os.path.basename(file_name).replace("%20", " "),
                 humanbytes(total_length),
