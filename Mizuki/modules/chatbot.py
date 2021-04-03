0094f85ff7e7c34a3d8fc6293dd890ac2b559b0e
lalbiakdikatochhawng <79501915+lalbiakdikatochhawng@users.noreply.github.com>
2021-04-03 12:43:00 +0530

    Update chatbot.py

--- Mizuki/modules/chatbot.py
+++ Mizuki/modules/chatbot.py
@@ -137,7 +137,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows mizuhara to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows lynntelo to talk and provides a more interactive group chat experience.
 
 *Commands:* 
 *Admins only:*
