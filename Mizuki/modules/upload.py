1c9866030ef1a87015eca6aac25c9d87d8187948
lalbiakdikatochhawng <79501915+lalbiakdikatochhawng@users.noreply.github.com>
2021-04-03 13:39:58 +0530

    Update upload.py

--- Mizuki/modules/upload.py
+++ Mizuki/modules/upload.py
@@ -184,7 +184,7 @@ __help__ = """
  ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link
  ⦁ `/tmpninja`*:* reply to a telegram file to upload it on tmpninja and get direct download link
 
-© @TheMizukiBot 👸
+© @Mizukimizo_bot 👸
 """
 
 __mod_name__ = "File To Link 🔗"
