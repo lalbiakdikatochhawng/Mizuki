f2d543d9b66503d4463ce63b87bf151ea9f0debd
lalbiakdikatochhawng <79501915+lalbiakdikatochhawng@users.noreply.github.com>
2021-04-03 17:25:13 +0530

    Update renamerhelp.py

--- Mizuki/modules/renamerhelp.py
+++ Mizuki/modules/renamerhelp.py
@@ -6,6 +6,6 @@ Just reply to file with below commands*
 • `/rename newfilename.extension` - rename media in as file
 • `/rename_video newfilename.extension` - rename media as streamable video
 
-© @Mizukimizo_bot 👸"""
+© @TheMizukiBot 👸"""
 
 __mod_name__ = "File Renamer ✏"
